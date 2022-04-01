9e2cdabb3c611a3a305ccd307682d8f0ad9586d6
Dmitriy Ignatenko <titarx@gmail.com>
2022-04-02 01:59:31 +0400

    Textarea readonly. Focus default

--- src/main/java/pro/corecode/passwordgenerator/MainApplication.java
+++ src/main/java/pro/corecode/passwordgenerator/MainApplication.java
@@ -3,6 +3,7 @@ package pro.corecode.passwordgenerator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -12,7 +13,11 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        Button generateButton = (Button) scene.lookup("#generateButton");
+        generateButton.requestFocus();
+
         stage.setTitle("Password generator");
         stage.getIcons().add(new Image("file:graphics/favicon-32x32.png"));
         stage.setScene(scene);
